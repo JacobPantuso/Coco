bc6c18dc3834c2af106677022c4126abf0fb517d
Jacob Pantuso <jacobpantuso@outlook.com>
2022-02-14 17:41:05 -0500

    Adding a launch screen

---
+++ Coco/launchScreen.swift
@@ -0,0 +1,20 @@
+//
+//  launchScreen.swift
+//  Coco
+//
+//  Created by Jacob Pantuso on 2022-02-14.
+//
+
+import SwiftUI
+
+struct launchScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct launchScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        launchScreen()
+    }
+}
