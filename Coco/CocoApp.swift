bd1bdffca3f203ad50f8f467f3fed87ae7a6b681
Jacob Pantuso <jacobpantuso@outlook.com>
2022-02-14 17:36:36 -0500

    Initial Commit

---
+++ Coco/CocoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocoApp.swift
+//  Coco
+//
+//  Created by Jacob Pantuso on 2022-02-14.
+//
+
+import SwiftUI
+
+@main
+struct CocoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
